77ac9612fb3e2d385abefa91e6e5b92d98b437af
MrinalBera <127860646+MrinalBera@users.noreply.github.com>
2023-06-27 14:00:21 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,53 @@
+class Person{
+	static String getName(){
+		return "Mrinal";
+	}
+	
+	static int getAge(){
+		return 22;
+	}
+	
+	static String getEmail(){
+		return "mrinalbera971@gmail.com";
+	}
+	
+	static long getMobileNo(){
+		return 7338184580L;
+	}
+	
+	static int getHeight(){
+		return 162;
+	}
+	
+	static int getWeight(){
+		return 67;
+	}
+	
+	static double getSalary(){
+		return 340000.00d;
+	}
+	
+	static String getBestFriendName(){
+		return "Srujan";
+	}
+	
+	static String getFatherName(){
+		return "Pratap";
+	}
+	
+	static String getMotherName(){
+		return "Meena";
+	}
+	
+	static String getArea(){
+		return "Hassan";
+	}
+	
+	static String getEnemy(){
+		return "juice";
+	}
+	
+	static String getInspiration(){
+		return "Brother";
+	}
+}
